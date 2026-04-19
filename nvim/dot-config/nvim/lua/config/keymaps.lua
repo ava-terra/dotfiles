832ef0efed61caad2b3106a5dbdc1fd1cef507e7
Avad <avad@avaterra.net>
2026-04-19 23:41:18 +0100

    Commented out ; to cmd mapping to not mask the neovim mapping

--- nvim/dot-config/nvim/lua/config/keymaps.lua
+++ nvim/dot-config/nvim/lua/config/keymaps.lua
@@ -3,6 +3,6 @@
 -- Add any additional keymaps here
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
+-- map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("i", "jj", "<ESC>")
